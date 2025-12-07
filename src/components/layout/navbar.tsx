7228147741a12ad69ded69dbabb1f6df6abe57d5
Tim Morgner <tim@timmorgner.de>
2025-12-07 06:47:14 +0100

    fix: remove empty NavigationMenuItem workaround and clean up Link components

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+// Wenn wir hier nicht "use client" nutzen ist das erste NavigationMenuItem einfach Leer... fml
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
@@ -5,31 +7,29 @@ export function Navbar() {
     return (
         <NavigationMenu>
                 <NavigationMenuList className="flex flex-row gap-4">
-                    <NavigationMenuItem x-devcomment="Ganz komischer Bug wo in Prod das erste Item leer ist, in Dev aber nicht. Daher dieses leere Item als Workaround.">
-                    </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/works" passHref>Werke</Link>
+                            <Link href="/works">Werke</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/collections" passHref>Sammlungen</Link>
+                            <Link href="/collections">Sammlungen</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/contests" passHref>Wettbewerbe</Link>
+                            <Link href="/contests">Wettbewerbe</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/pennames" passHref>Pseudonyme</Link>
+                            <Link href="/pennames">Pseudonyme</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/users" passHref>Benutzer</Link>
+                            <Link href="/users">Benutzer</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                 </NavigationMenuList>
